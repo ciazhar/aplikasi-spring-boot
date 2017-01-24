4fd3f6c6fe6ce801362145cc91d7fc8b7530ab57
ciazhar <daisetsunakama@gmail.com>
2017-01-24 08:51:51 +0700

    fix security login

---
+++ src/main/java/com/ciazhar/config/KonfigurasiSecurity.java
@@ -0,0 +1,61 @@
+package com.ciazhar.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+@EnableWebMvc
+public class KonfigurasiSecurity extends WebSecurityConfigurerAdapter{
+    private static final String SQL_LOGIN
+            = "SELECT username,password, enable " +
+            "FROM s_users WHERE username = ?";
+
+    private static final String SQL_PERMISSION
+            = "SELECT u.username, r.nama as authority " +
+            "FROM s_users u " +
+            "JOIN s_user_role ur on u.id = ur.id_user " +
+            "JOIN s_roles r on ur.id_role = r.id " +
+            "WHERE u.username = ?";
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+
+        //setting security non database
+        auth
+                .inMemoryAuthentication()
+                .withUser("ciazhar")
+                .password("123")
+                .roles("apa");
+
+        ///Setting security database
+        /*auth
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(SQL_LOGIN)
+                .authoritiesByUsernameQuery(SQL_PERMISSION);*/
+    }
+
+    ///konfigurasi web mana yg boleh diakses admin staf user dll
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .permitAll()
+                .and()
+                .logout();
+    }
+}
---
+++ src/main/java/com/ciazhar/config/KonfigurasiWeb.java
@@ -0,0 +1,21 @@
+package com.ciazhar.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.jasperreports.JasperReportsMultiFormatView;
+import org.springframework.web.servlet.view.jasperreports.JasperReportsViewResolver;
+
+/**
+ * Created by hafidz on 08/01/17.
+ */
+@Configuration
+public class KonfigurasiWeb extends WebMvcConfigurerAdapter{
+
+    ///konfigurasi page yg gak ada controllernya tapi mau di UI
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/login").setViewName("login");
+    }
+}
