80c4389e42e6daae4dd5a7b0b6039636021c28f0
ciazhar <daisetsunakama@gmail.com>
2017-02-19 12:14:49 +0700

    fix typo

--- src/main/java/com/ciazhar/controller/HaloController.java
+++ src/main/java/com/ciazhar/controller/HaloController.java
@@ -1,4 +1,4 @@
-package com.ciazhar.spring.boot.controller;
+package com.ciazhar.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
---
+++ src/main/java/com/ciazhar/controller/MateriController.java
@@ -0,0 +1,5 @@
+package com.ciazhar.controller;
+
+public class MateriController{
+
+}
