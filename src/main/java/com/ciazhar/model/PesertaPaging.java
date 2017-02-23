19913d50baa1848ed44c26ddd2f3e64d7abc62a0
ciazhar <daisetsunakama@gmail.com>
2017-02-23 21:24:45 +0700

    buat model peserta baru

---
+++ src/main/java/com/ciazhar/model/PesertaPaging.java
@@ -0,0 +1,38 @@
+package com.ciazhar.model;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity @Table(name = "t_peserta")
+public class PesertaPaging {
+
+    @Id @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    @NotNull @NotEmpty @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull @NotEmpty
+    private String nama;
+
+    @Column(name = "nomor_handphone")
+    private String nomorHandphone;
+
+    @NotNull @Past
+    @Column(name = "tanggal_lahir")
+    @Temporal(TemporalType.DATE)
+    private Date tanggalLahir;
+}
