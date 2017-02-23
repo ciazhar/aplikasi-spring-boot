14cb07a7b6c5816f564c7829a27676ba50d73c32
ciazhar <daisetsunakama@gmail.com>
2017-02-23 23:55:30 +0700

    peserta paging

---
+++ src/main/java/com/ciazhar/controller/PesertaPagingController.java
@@ -0,0 +1,22 @@
+package com.ciazhar.controllers;
+
+
+
+import com.ciazhar.model.PesertaPaging;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PesertaPagingController {
+
+
+
+    @RequestMapping("/peserta/registrasi")
+    public void registrasi(){}
+
+
+}
