c206bf8e5286d8e61382363c44dced2de967f107
ciazhar <daisetsunakama@gmail.com>
2017-02-24 08:44:39 +0700

    form input

--- src/main/java/com/ciazhar/controller/PesertaPagingController.java
+++ src/main/java/com/ciazhar/controller/PesertaPagingController.java
@@ -3,39 +3,66 @@ package com.ciazhar.controllers;
 
 import com.ciazhar.dao.PesertaPagingDao;
 import com.ciazhar.model.PesertaPaging;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.support.SessionStatus;
 
 @Controller
 public class PesertaPagingController {
 
     @Autowired private PesertaPagingDao pesertaPagingDao;
 
-    @RequestMapping("/peserta/registrasi")
+    @RequestMapping("/peserta/registrasi/")
     public void registrasi(){}
 
-    @RequestMapping("/api/peserta")
+    @RequestMapping("/api/peserta/")
     @ResponseBody
     public Page<PesertaPaging> semuaPeserta(Pageable page){
         return pesertaPagingDao.findAll(page);
     }
 
-    @RequestMapping("/api/peserta/{id}")
+    @RequestMapping("/api/peserta/{id}/")
     @ResponseBody
     public PesertaPaging cariById(@PathVariable(name = "id") PesertaPaging peserta){
         return peserta;
     }
 
-    @RequestMapping("/peserta/list")
+    @RequestMapping("/peserta/list/")
     public ModelMap daftarPeserta(Pageable page){
         ModelMap data = new ModelMap();
         data.put("daftarPeserta", pesertaPagingDao.findAll(page));
         return data;
+    }
+    @RequestMapping(value="/peserta/form/", method = RequestMethod.GET)
+    public ModelMap tampilkanForm(@RequestParam(required = false, name = "id") PesertaPaging p){
+        ModelMap data = new ModelMap();
+
+        if(p == null){
+            p = new PesertaPaging();
+        }
+
+        data.put("peserta", p);
+
+        return data;
+    }
+    @RequestMapping(value = "/peserta/form/", method = RequestMethod.POST)
+    public String prosesForm(@ModelAttribute @Valid PesertaPaging p, BindingResult errors, SessionStatus status){
+        if(errors.hasErrors()) {
+            return "/peserta/form";
+        }
+
+        status.setComplete();
+        return "redirect:/peserta/list/";
     }
 }
