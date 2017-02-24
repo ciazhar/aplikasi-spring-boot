cdd2d419bc0d5686ac798343c958cc3a17aee29a
ciazhar <daisetsunakama@gmail.com>
2017-02-24 07:47:00 +0700

    menambahakan methode caribyid

--- src/main/java/com/ciazhar/controller/PesertaPagingController.java
+++ src/main/java/com/ciazhar/controller/PesertaPagingController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -18,9 +19,15 @@ public class PesertaPagingController {
     @RequestMapping("/peserta/registrasi")
     public void registrasi(){}
 
-    @RequestMapping("/api/peserta/")
+    @RequestMapping("/api/peserta")
     @ResponseBody
     public Page<PesertaPaging> semuaPeserta(Pageable page){
         return pesertaPagingDao.findAll(page);
+    }
+
+    @RequestMapping("/api/peserta/{id}")
+    @ResponseBody
+    public PesertaPaging cariById(@PathVariable(name = "id") PesertaPaging peserta){
+        return peserta;
     }
 }
