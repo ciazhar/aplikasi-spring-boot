59a080e6606ea56ee618698684c5a51d39913d13
ciazhar <daisetsunakama@gmail.com>
2017-02-27 17:02:46 +0700

    membuat materi dao

---
+++ src/main/java/com/ciazhar/dao/MateriDao.java
@@ -0,0 +1,8 @@
+package com.ciazhar.dao;
+
+import com.ciazhar.model.Materi;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface MateriDao extends PagingAndSortingRepository<Materi,String>{
+
+}
